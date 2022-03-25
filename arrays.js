e78e7b6d67a9750c1628e90779ea351a49641041
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-03-25 04:10:06 +0000

    Completed functions

--- arrays.js
+++ arrays.js
@@ -2,15 +2,35 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(items[i], i);
+  }
 }
+
+// printIndices(['apple', 'berry', 'cherry'])
+
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
+
+// everyOtherItem(['apple', 'berry', 'cherry'])
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  items.sort((a, b) => a - b);
+  // console.log(items);
+  // console.log(items);
+  const newArray = items.slice(0, n);
+  newArray.reverse();
+  console.log(newArray)
 }
+
+// smallestNItems([1, 30, 4, 21, 10000], 2)
