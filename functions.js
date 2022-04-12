f6ec2651ca53a4a271f6bd367364ce7ce7250231
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-04-12 19:57:05 +0000

    Completed functions.js

--- functions.js
+++ functions.js
@@ -2,12 +2,42 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+    return town === 'San Francisco'
+}
+
+// console.log(isHometown('Atlanta'))
+// console.log(isHometown('San Francisco'))
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
+// console.log(getFullName('Andrea', 'Dashe'))
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+    let subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+
+    if (state === 'CA') {
+      fee = 0.03 * subtotal;
+    } else if (state === 'PA') {
+      fee = 2;
+    } else if (state === 'MA') {
+      if (basePrice <= 100) {
+        fee = 1;
+      } else {
+        fee = 3;
+      }
+    }
+    return (subtotal + fee)
+}
+
+console.log(calculateTotal(1.00, 'CA'))
+console.log(calculateTotal(1.00, 'PA'))
+console.log(calculateTotal(1.00, 'MA'))
